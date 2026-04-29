9957274da73833e8adba38338f429f74d5c95346
gattihhazleoiu <gattihhazleoiu@hotmail.com>
2026-04-29 14:21:43 -0300

    Update button to 'Login' and link to /login

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -133,11 +133,11 @@ function Header() {
         </nav>
 
         <Link
-          to="/cadastro"
+          to="/login"
           className="beam relative rounded-full px-4 py-2 text-[14px] font-medium text-white/90 bg-white/[0.03] border border-white/10"
           style={{ letterSpacing: '-0.01em' }}
         >
-          Começar agora
+          Login
         </Link>
       </div>
     </header>
